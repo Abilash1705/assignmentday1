37836bc36cde067493675c0974073b9a993bcf10
Abilash1705 <111682231+Abilash1705@users.noreply.github.com>
2022-10-11 10:36:44 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Mobile {
+	String mobilename="Apple";
+	char firstLetter='A';
+	long mobileNumber=5646249266L;
+	float mobilePrice=50336.613F;
+	boolean isWorking=true;
+	int serialNumber=864646;
+	short noOfpiece=50;
+	public static void main(String[] args) {
+		Mobile phone=new Mobile();
+		System.out.println(phone.mobilename);
+		System.out.println(phone.firstLetter);
+		System.out.println(phone.mobileNumber);
+		System.out.println(phone.mobilePrice);
+		System.out.println(phone.isWorking);
+		System.out.println(phone.serialNumber);
+		System.out.println(phone.noOfpiece);
+	}
+	
+	
+			
+			
+
+}
---
+++ Twowheeler.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class Twowheeler {
+	int noOfwheels=2;
+	short noOfmirrors=2;
+	long registerNumber=461314;
+	boolean isBroken=false;
+	double kilometerDriven=81545;
+	String bikeName="triumph";
+	public static void main(String[] args) {
+		Twowheeler bike=new Twowheeler();
+		System.out.println(bike.noOfwheels);
+		System.out.println(bike.noOfmirrors);
+		System.out.println(bike.isBroken);
+		System.out.println(bike.kilometerDriven);
+		System.out.println(bike.bikeName);
+
+		
+
+	
+	}
+	
+		
+	
+		
+
+}
